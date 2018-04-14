d5fab78cb81f4673d6d55eb1ef0d3f7bc6effb3d
nienkebos <nienkebos@live.com>
2018-04-14 14:47:38 +0200

    Add firebase connection

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyC9UuqipBkt3u3XufwY_vVeXjDbZpHx1MU",
+    authDomain: "mahjongscores-3a1cb.firebaseapp.com",
+    databaseURL: "https://mahjongscores-3a1cb.firebaseio.com",
+    projectId: "mahjongscores-3a1cb",
+    storageBucket: "mahjongscores-3a1cb.appspot.com",
+    messagingSenderId: "656166531709"
+};
+
+firebase.initializeApp(config);
+
+export default firebase;
+
+export const database = firebase.database();
