d06e9f08f81d58c44451a3d126556f51c152bab9
nienkebos <nienkebos@live.com>
2018-04-15 16:28:02 +0200

    Add GameRack component

---
+++ src/Game/GameRack.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './GameRack.css';
+
+class GameRack extends React.Component {
+
+    render() {
+        return(
+            <div className="Game-rack">GameRack</div>
+        )
+    }
+}
+
+export default GameRack;
